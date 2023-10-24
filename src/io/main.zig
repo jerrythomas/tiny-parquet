200f3864ba2d3e07a7051c949f4196e1e82de313
Jerry Thomas <me@jerrythomas.name>
2023-10-24 18:42:19 +0530

    fix: added missing tests in io module

--- src/io/main.zig
+++ src/io/main.zig
@@ -1,7 +1,10 @@
-pub const Reader = @import("Reader.zig").Reader;
 pub const FileReader = @import("FileReader.zig").FileReader;
 pub const S3FileReader = @import("S3FileReader.zig").S3FileReader;
 
+pub const Reader = @import("Reader.zig").Reader;
+
 test {
+    _ = FileReader;
+    _ = S3FileReader;
     _ = Reader;
 }
